4ea8b8d41b200b2afb6aa4ca77250ffea365fdd1
Jackson Morgan <morganja@us.ibm.com>
2016-06-06 15:14:31 -0400

    Props

--- src/client.js
+++ src/client.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
 ReactDOM.render((
-	  	<App />
+	  	<App name="World" />
   ),
   document.getElementById('app')
 );
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
-  mixins: [PureRenderMixin],
-
 
   render: function() {
     return (
       <div className="app">
-        <h1>Good Afternoon. This is my App</h1>
+        <h1>Hello {this.props.name}</h1>
       </div>
     );
   }
